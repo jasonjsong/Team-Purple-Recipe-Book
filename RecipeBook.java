84edd9c0a71982ffa665e353d9ca9040f4ef4e6e
J. Hernandez <jhy391@nyu.edu>
2022-10-03 18:47:43 -0400

    created interface of choosing between adding a recipe and searching through the recipe book

--- RecipeBook.java
+++ RecipeBook.java
@@ -1,1 +1,21 @@
+import java.util.*;
 
+public class RecipeBook {
+
+  public static void main(String[] args) {
+    Scanner readInput = new Scanner(System.in);
+
+    System.out.println("Do you wish to add a new recipe, or search through the existing cookbook?\nPress 'A' to add a new recipe, or 'S' to search: ");
+    String addOrSearch = readInput.nextLine();
+    if (addOrSearch.equals("A")) {
+      System.out.println("Creating new recipe: ");
+      //TODO: add more input options for title, description, etc. once Recipe Object is created
+    }
+    if (addOrSearch.equals("S")) {
+      System.out.println("Type in what you want to search for, then press Enter: ");
+      String searching = readInput.nextLine();
+      System.out.println("Searching for " + searching);
+    }
+
+  }
+}
