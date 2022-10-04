6bb79dfb0141b0cb810d13b5160ee51dc17f9973
J. Hernandez <jhy391@nyu.edu>
2022-10-03 21:56:26 -0400

    fixed up the comments and added a welcome message

--- RecipeBook.java
+++ RecipeBook.java
@@ -5,11 +5,12 @@ public class RecipeBook {
   public static void main(String[] args) {
     Scanner readInput = new Scanner(System.in);
 
+    System.out.println("Welcome to Team Purple's Recipe Book!");
     System.out.println("Do you wish to add a new recipe, or search through the existing cookbook?\nPress 'A' to add a new recipe, or 'S' to search: ");
     String addOrSearch = readInput.nextLine();
     if (addOrSearch.equals("A")) {
       System.out.println("Creating new recipe: ");
-      //TODO: add more input options for title, description, etc. once Recipe Object is created
+      //TODO: create recipe object and use input options for title, description, etc.
     }
     if (addOrSearch.equals("S")) {
       System.out.println("Type in what you want to search for, then press Enter: ");
