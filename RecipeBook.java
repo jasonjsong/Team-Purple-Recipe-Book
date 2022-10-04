16e838ac9a8545f771ece7423bd04f5a6ca6bbba
HenryGreene10 <90366384+HenryGreene10@users.noreply.github.com>
2022-10-04 11:32:53 -0400

    Update RecipeBook.java

--- RecipeBook.java
+++ RecipeBook.java
@@ -5,6 +5,8 @@ public class RecipeBook {
   public static void main(String[] args) {
     Scanner readInput = new Scanner(System.in);
 
+    ArrayList<recipe> book = new ArrayList<recipe>();
+
     System.out.println("Do you wish to add a new recipe, or search through the existing cookbook?\nPress 'A' to add a new recipe, or 'S' to search: ");
     String addOrSearch = readInput.nextLine();
     if (addOrSearch.equals("A")) {
@@ -16,6 +18,28 @@ public class RecipeBook {
       String searching = readInput.nextLine();
       System.out.println("Searching for " + searching);
     }
-
+    
+    public sortAlphabetically(ArrayList recipes){
+        //length = size(recipes);
+        //for (i=0; i<recipes; i++){ }
+        recipes = Collections.sort(recpies);
+    }
   }
+}
+
+public class recipe{
+    String title;
+    String description;
+    ArrayList<String> ingredients = new ArrayList<String>();
+    ArrayList<String> steps = new ArrayList<String>();
+    
+    public recipe(){
+    }
+
+    public recipe(String title, String description, ArrayList ingredients, ArrayList steps){
+        title = title;
+        description = description;
+        ingredients = ingredients;
+        steps = steps;
+    }
 }
